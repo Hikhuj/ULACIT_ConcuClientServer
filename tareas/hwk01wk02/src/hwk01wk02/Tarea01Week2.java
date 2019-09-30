04a5ad0e770faaaf6b87f592557b9c6ddedac43b
Roger Ulate <rulater530@ulacit.ed.cr>
2019-09-30 00:02:28 -0600

    Tarea 1, completa

--- tareas/hwk01wk02/src/hwk01wk02/Tarea01Week2.java
+++ tareas/hwk01wk02/src/hwk01wk02/Tarea01Week2.java
@@ -5,6 +5,8 @@
  */
 package hwk01wk02;
 
+import java.util.*;
+
 /**
  *
  * @author roger
@@ -14,20 +16,35 @@ public class Tarea01Week2 {
     /**
      * @param args the command line arguments
      */
+    
+    // Arerglo de Animales declarado
+    public static ArrayList<Animal> animales = new ArrayList<Animal>();
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // Ejemplo de herencia con animales
         
         /*
-            Armas
-                fabricante
-                numero de serie
-                calibre
-                tipo de arma
-        
-            Armas cortas, largas, deportivas
-                corta automatica, manual | 
+            Ejemplo de Herencia con Animales
         */
         
+        // Inicializacion de objetos
+        Perro perroPastor = new Perro(12.5f, 20.6f, "Caninus Perrus", "12 anios", 12, "Pastor Aleman", "Lobo");
+        Aguila aguilaCalva = new Aguila(20.5f, 12.6f, "Aguilus Calvus", "25 anios", 45.5f, "Veripara", true);
+        Tiburon tiburonMartillo = new Tiburon(120f, 50.5f, "Tiburonos Sangrientos", "25 anios", "Salada", 1500, 50);
+        
+        // Insercion de animales a arreglo
+        animales.add(perroPastor);
+        animales.add(aguilaCalva);
+        animales.add(tiburonMartillo);
+       
+        // Impresion basica
+        System.out.println("Todos los animales comienza a nacer" + "Todos ejecutan el mismo metodo");
+        
+        // Impresion de todos los animales por medio de una iteracion
+        for(Animal a: animales){
+            System.out.println(a.getNombreCientifico() + ", " + a.getPeso() + ", " + a.mostrarFrase());
+        }
         
     }
     
