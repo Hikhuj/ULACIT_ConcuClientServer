47bd6181f4e5d112bbbe2ffe6532f3d6395d69ec
Róger José Ulate Rivera <roger.p.designe357@gmail.com>
2019-10-28 01:30:55 -0600

    Leccion 6 (Sockets)

---
+++ clases/Lec06/src/lec06/Client.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lec06;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author rogerjoseulaterivera
+ */
+public class Client {
+    
+    private static final String ip = "127.0.0.1";
+    private static final int port = 5432;
+    
+    public static void main(String[] args){
+        Socket s1 = null;
+        
+        try {
+            s1 = new Socket(ip, port);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error");
+        }
+        
+        DataInputStream dis;
+        DataOutputStream dos;
+        
+        try{
+            dos = new DataOutputStream(s1.getOutputStream());
+            dis = new DataInputStream(s1.getInputStream());
+            dos.writeUTF("Roger Ulate Rivera");
+            System.out.println(dis.readUTF());
+            dis.close();
+            s1.close();
+        }catch (IOException ex){
+           Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+           System.out.println("Error");
+        }
+        
+    }
+    
+}
