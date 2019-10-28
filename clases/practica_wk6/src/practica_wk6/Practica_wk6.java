9fe9d8bb043d9be5d649c561e2282a881b05668c
Róger José Ulate Rivera <roger.p.designe357@gmail.com>
2019-10-28 01:30:14 -0600

    Varios

--- clases/practica_wk6/src/practica_wk6/Practica_wk6.java
+++ clases/practica_wk6/src/practica_wk6/Practica_wk6.java
@@ -17,9 +17,28 @@ public class Practica_wk6 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        cliente clientes = new cliente();
+        Hilo hilo = new Hilo();
         
+        int limiteSuperior = 300;
+        int limiteInferior = 600;
         
+        int cantidad = (int)((limiteSuperior - limiteInferior + 1) * Math.random() + limiteInferior);
+        int cantidad2 = (int)((limiteSuperior - limiteInferior + 1) * Math.random() + limiteInferior);
+        
+        System.out.println(cantidad2);
+        
+        hilo.cantidadPasillos(6);
+        
+        //hilo.start();
+        
+        for (int i = 0; i < 100; i++) {
+            hilo.start();
+            try{
+                hilo.sleep(cantidad2);
+            }catch (InterruptedException ex){
+                System.out.println("Nada pasa");
+            }
+        }
         
     }
     
